f03a634557c21a8c399c02156ea173c15fefb6bd
Onelky <onelkt.hernandez29@gmail.com>
2021-12-03 19:32:53 -0400

    Add kelvin and celcius conversion

--- app.py
+++ app.py
@@ -20,9 +20,28 @@ class Temperature:
 
     def to_f(self):
         if(self.type == TemperatureScale.C):
-            return (self.value * 9/5) + 32
+            return round((self.value * 9/5) + 32, 2)
 
         elif(self.type == TemperatureScale.K):
-            return (self.value - 273.15) * 9/5 + 32
+            return round((self.value - 273.15) * 9/5 + 32, 2)
 
         return self.value
+    
+    def to_k(self):
+        if(self.type == TemperatureScale.C):
+            return round(self.value + 273.15)
+
+        elif(self.type == TemperatureScale.F):
+            return 273.5 + ((self.value - 32.0) * (5.0/9.0))
+
+        return self.value
+    
+    def to_c(self):
+        if(self.type == TemperatureScale.F):
+            return round((self.value - 32) * 5/9, 2)
+
+        elif(self.type == TemperatureScale.K):
+            return round(self.value - 273.15, 2)
+
+        return self.value
+       
--- test_cases.py
+++ test_cases.py
@@ -1,9 +1,32 @@
 from app import Temperature
 from temp_scale import TemperatureScale
 
-def test_temptoF_0C_should_return_32F():
+
+def test_to_f_0C_should_return_32F():
     celcius_temp = Temperature(0, TemperatureScale.C)
     assert celcius_temp.to_f() == 32
-    
 
-     
+
+def test_to_k_0C_should_return_273K():
+    celcius_temp = Temperature(0, TemperatureScale.C)
+    assert celcius_temp.to_k() == 273.15
+
+
+def test_to_c_32F_should_return_0C():
+    fahrenheit_temp = Temperature(32, TemperatureScale.F)
+    assert fahrenheit_temp.to_c() == 0
+
+
+def test_to_k_32F_should_return_273_15K():
+    fahrenheit_temp = Temperature(32, TemperatureScale.F)
+    assert fahrenheit_temp.to_k() == 273.15
+
+
+def test_to_c_300K_should_return_26_85C():
+    kelvin_temp = Temperature(300, TemperatureScale.K)
+    assert kelvin_temp.to_c() == 26.85
+
+
+def test_to_f_300K_should_return_80_33F():
+    kelvin_temp = Temperature(300, TemperatureScale.K)
+    assert kelvin_temp.to_f() == 80.33
